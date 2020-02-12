8e7ff229a5dd82f70604f31ff646a9f8e2ff136b
RJWadley <robbiethewadley@gmail.com>
2020-02-11 23:55:48 -0600

    Update and rename final.bat to final-1.bat

--- final-1.bat
+++ final-1.bat
@@ -4,4 +4,4 @@ curl https://raw.githubusercontent.com/RJWadley/mmc-launch/master/JREPL.BAT --ou
 set DispName="Scarycrumb45"
 rem Ask user of batch file for the number of users.
 set /P "DispName=Enter Display Name (%DispName%): "
-jrepl.bat "Scarycrumb45" "%DispName%" /f launcher_profiles.json /o - & cd "C:\Program Files (x86)\Minecraft Launcher\" && start MinecraftLauncher.exe
+jrepl.bat "Scarycrumb45" "%DispName%" /f launcher_profiles.json /o - && cd "C:\Program Files (x86)\Minecraft Launcher\" && start MinecraftLauncher.exe
