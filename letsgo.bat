2357dc52c4014b79b57e565c575c46bbf5cfe648
RJWadley <robbiethewadley@gmail.com>
2020-02-11 23:13:56 -0600

    Update letsgo.bat

--- letsgo.bat
+++ letsgo.bat
@@ -4,4 +4,4 @@ rem Ask user of batch file for the number of users.
 set /P "DispName=Enter Display Name (%DispName%): "
 
 jrepl.bat "Scarycrumb45" "%DispName%" /f launcher_profiles.json /o -
-start "C:\Program Files (x86)\Minecraft Launcher"
+start "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
